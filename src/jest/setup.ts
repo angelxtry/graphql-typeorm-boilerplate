3667cf7e4406bb563b7ee0f27d4dcac53dcf7f9d
LeeSuHo <angelxtry@gmail.com>
2019-12-20 01:38:07 +0900

    Change port

--- src/jest/setup.ts
+++ src/jest/setup.ts
@@ -2,5 +2,5 @@ import { startServer } from '../startServer';
 
 export const setup = (async () => {
   await startServer();
-  process.env.TEST_HOST = 'http://localhost:4000/graphql';
+  process.env.TEST_HOST = 'http://localhost:3000/graphql';
 });
--- src/startServer.ts
+++ src/startServer.ts
@@ -44,7 +44,7 @@ export const startServer = async () => {
     cors,
   });
 
-  app.listen({ port: 4000 }, () => {
+  app.listen({ port: 3000 }, () => {
     console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
   });
 };
